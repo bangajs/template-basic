01503676f0ef4b715554fbff3bbc09126f44f5c6
saucecodee <ashawinta@gmail.com>
2020-03-13 09:43:54 +0100

    Cleaned up the index routes code

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,11 @@
 const router = require("express").Router();
 const userRoutes = require("./userRoutes");
-const doctorRoutes = require("./doctorRoutes");
-const appointmentRoutes = require("./appointmentRoutes");
-const specializationRoutes = require("./specializationRoutes");
-const symptomRoutes = require("./symptomRoutes");
 
-module.exports = function(app) {
+module.exports = function() {
   router.get("/test", (req, res) => {
     res.send("yehh!");
   });
   router.use("/users", userRoutes());
-  router.use("/doctors", doctorRoutes());
-  router.use("/appointments", appointmentRoutes());
-  router.use("/specializations", specializationRoutes());
-  router.use("/symptoms", symptomRoutes());
 
   return router;
 };
