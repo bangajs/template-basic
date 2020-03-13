fb6b4e0a3e6e143f20df2c8d95837ac7d0febc11
saucecodee <ashawinta@gmail.com>
2020-03-13 09:48:00 +0100

    Clean up uneccessary codes in the user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,6 @@ const {
   signinUser,
   getUsers,
   getUser,
-  getUserAppointments,
   deleteUser,
   editUser,
 } = require("../services/userServices");
@@ -12,40 +11,35 @@ const { response } = require("../helpers/messages");
 const CustomError = require('../helpers/CustomError')
 
 class UserContoller {
-  async signupUser(req, res, next) {
+  async signupUser(req, res) {
     const token = await signupUser(req.body);
-    res.status(201).send(response("Account created", token));
+    res.status(201).send(response("user created", token));
   }
 
-  async signinUser(req, res, next) {
+  async signinUser(req, res) {
     const token = await signinUser(req.body);
-    res.status(200).send(response("User signed in", token));
+    res.status(200).send(response("user signed in", token));
   }
 
-  async getUsers(req, res, next) {
+  async getUsers(req, res) {
     const users = await getUsers();
-    res.status(200).send(response("All users", users));
+    res.status(200).send(response("all users", users));
   }
 
-  async getUser(req, res, next) {
+  async getUser(req, res) {
     const user = await getUser(req.params.userId);
-    res.status(200).send(response("User detail", user));
+    res.status(200).send(response("user data", user));
   }
 
-  async getUserAppointments(req, res, next) {
-    const userAppointments = await getUserAppointments(req.params.userId);
-    res.status(200).send(response(`User's appointments`, userAppointments));
-  }
-
-  async editUser(req, res, next) {
+  async editUser(req, res) {
     const user = await editUser(req.params.userId, req.body);
     if (req.params.userId != req.headers.user.id) throw new CustomError("Invalid user", 401)
-    res.status(200).send(response("Profile edited", user));
+    res.status(200).send(response("user edited", user));
   }
 
-  async deleteUser(req, res, next) {
+  async deleteUser(req, res) {
     const user = await deleteUser(req.params.userId);
-    res.status(200).send(response("User deleted", user));
+    res.status(200).send(response("user deleted", user));
   }
 }
 
