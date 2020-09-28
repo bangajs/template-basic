3512bb600a7a167a014544ac0ce8f7cf674e3f84
saucecodee <ashawinta@gmail.com>
2020-09-28 16:48:00 +0100

    Refactor: Roles and authentication

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,17 +1,25 @@
 const JWT = require("jsonwebtoken");
 const User = require("./../models/user.model");
 const CustomError = require("./../utils/custom-error");
+const { role } = require("./../configs/default.config")
 
+/**
+ * If no role is passed the default role is user
+ * 
+ * @param  {any[]} roles List of roles allowed to access the route
+ */
 function auth(...roles) {
+     roles = roles.length > 0 ? roles : role.USER
+
      return async (req, res, next) => {
-          if (!req.headers.authorization) throw new CustomError("Token not found", 401);
+          if (!req.headers.authorization) throw new CustomError("Unauthorized access: Token not found", 401);
 
           const token = req.headers.authorization.split(" ")[1];
-          const decoded =  JWT.verify(token, process.env.JWT_SECRET);
+          const decoded = JWT.verify(token, process.env.JWT_SECRET);
 
           let user = await User.findOne({ _id: decoded.id });
-          if (!user || !user.isActive) throw new CustomError("Unauthorized user", 401);
-          if (!roles.includes(result.role)) throw new CustomError("You don't have authorization to access the requested resource", 401)
+          if (!user || !user.isActive) throw new CustomError("Unauthorized access: User has been deactivated", 401);
+          if (!roles.includes(result.role)) throw new CustomError("Unauthorized access", 401)
 
           req.$user = user;
 
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,13 +1,14 @@
 const router = require("express").Router();
-const UserCtrl = require("../controllers/user.controller");
-const auth = require('../middlewares/auth.middleware');
+const UserCtrl = require("./../controllers/user.controller");
+const auth = require('./../middlewares/auth.middleware');
+const {p} = require("./../configs/default.config")
 
 
 router.post("/", UserCtrl.create);
 router.post("/login", UserCtrl.login);
 
-router.get("/", auth(), UserCtrl.getMany);
-router.get("/:userId", auth(), UserCtrl.getById);
+router.get("/", auth(), UserCtrl.getAll);
+router.get("/:userId", auth(), UserCtrl.getOne);
 
 router.put("/:userId", auth(), UserCtrl.update);
 
