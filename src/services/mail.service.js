c271cce85372872d8f5a8e77e586513a80a8cbdc
saucecodee <ashawinta@gmail.com>
2020-09-28 17:44:48 +0100

    Refactor mail service

--- src/services/mail.service.js
+++ src/services/mail.service.js
@@ -1,28 +1,33 @@
-const nodemailer = require('nodemailer')
-const config = require('../../config')[process.env.NODE_ENV]
-const templatingService = new (require('./TemplatingService'))();
+const nodemailer = require('nodemailer');
+const CustomError = require('./../utils/custom-error');
+const { MAILER_NAME, MAILER_HOST, MAILER_USER, MAILER_PASSWORD } = process.env
 
 class MailService {
      constructor() {
-          this.transporter = nodemailer.createTransport(config.mail);
+          this.transporter = nodemailer.createTransport({
+               name: MAILER_NAME,
+               host: MAILER_HOST,
+               port: 465,
+               secure: true,
+               auth: {
+                    user: MAILER_USER,
+                    pass: MAILER_PASSWORD
+               },
+          });
      }
 
-     getTemplate(context, template = "emails/default.jade") {
-          return templatingService.render(template, context);
-     };
-
-     async sendMail(from, to, subject, context, template) {
+     async send(from, to, subject, context, template) {
           from = from || '"no-reply XXX" <no-reply@xxx.xxx>'
           context = context || {}
 
-          if (!to) throw new Error("Recipient is required");
-          if (!subject) throw new Error("Subject is required");
+          if (!to) throw new CustomError("Recipient is required");
+          if (!subject) throw new CustomError("Subject is required");
 
           return await this.transporter.sendMail({
                from,
                to: Array.isArray(to) ? to.join() : to,
-               subject: subject,
-               html: this.getTemplate(context, template)
+               subject,
+               html: template
           });
      }
 }
