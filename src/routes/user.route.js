a01e6d96c4d2f31e5fa08867ba82a8a15b8c3270
saucecodee <ashawinta@gmail.com>
2020-10-11 09:48:28 +0100

    Feat:  Reset password, Verify Email and Change password

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -2,13 +2,25 @@ const router = require("express").Router();
 const UserCtrl = require("./../controllers/user.controller");
 const auth = require('./../middlewares/auth.middleware');
 const upload = require("./../middlewares/multer.middleware")
-const { role } = require("./../configs/default.config")
+const { role } = require("../config")
 
-
+//route - POST:: user/
+//desc - Create a new user
+//access - Public
 router.post("/", upload("image"), UserCtrl.create);
+
+//route - POST:: user/login
+//desc - Login a user
+//access - Public
 router.post("/login", UserCtrl.login);
-router.post("/verify-email", UserCtrl.login);
-router.post("/resend-verification-email", UserCtrl.login);
+
+//route - POST:: user/
+//desc - Create a new user
+//access - Public
+router.post("/request-email-verification", UserCtrl.RequestEmailVerification);
+router.post("/verify-email", UserCtrl.VerifyEmail);
+router.post("/request-password-reset", UserCtrl.login);
+router.post("/reset-password", UserCtrl.login);
 
 router.get("/", auth(role.USER), UserCtrl.getAll);
 router.get("/:userId", auth(role.USER), UserCtrl.getOne);
