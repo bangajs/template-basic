1230eb1f18b76f7c32dec3af7025eaa951648152
saucecodee <ashawinta@gmail.com>
2020-10-12 01:06:42 +0100

    Add mongo  dupicate key error handler

--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -1,8 +1,7 @@
-const CustomError = require("./../utils/custom-error");
 const response = require("./../utils/response")
 
 //Possible error names
-const errorNames = ["CastError", "JsonWebTokenError", "ValidationError", "SyntaxError", "MongooseError"]
+const errorNames = ["CastError", "JsonWebTokenError", "ValidationError", "SyntaxError", "MongooseError", "MongoError"]
 
 module.exports = (app) => {
      app.use("*", (req, res) => {
@@ -11,13 +10,21 @@ module.exports = (app) => {
 
      app.use((error, req, res, next) => {
           console.error(error)
-          
-          if (error instanceof CustomError)
+
+          if (error.name == "CustomError") {
                res.status(error.status).send(response(error.message, null, false));
-          else if (errorNames.includes(error.name))
+          }
+          else if (error.name == "MongoError" && error.code == 11000) {
+               // Catch duplicate key field
+               const field = Object.entries(error.keyValue)[0][0]
+               res.status(400).send(response(`${field} already exists`, null, false));
+          }
+          else if (errorNames.includes(error.name)) {
                res.status(400).send(response(error.message, null, false));
-          else
+          }
+          else {
                res.status(500).send(response(error.message, null, false));
+          }
      });
 
      return app
